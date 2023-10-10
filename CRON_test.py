1606b3064f11756de76370e1e495c296d4f5a969
Haydn Martin <haydnmartin6@gmail.com>
2023-10-10 09:38:31 +0100

    Public clean up

--- CRON_test.py
+++
@@ -1,3 +0,0 @@
-# Reading lines from txt file
-filename = open('Test.txt', 'a')
-filename.write('CRON_test\n')
--- auto_tweets.py
+++ auto_tweets.py
@@ -32,17 +32,17 @@ def save_too_long(tweet):
 # Check if already sent
 def sent_tweet_bool(tweet):
 
-    archive = open('Sent_Tweets2.txt', 'r')
+    archive = open('Sent_Tweets.txt', 'r')
     sent_bool = tweet in archive.readlines()
     archive.close()
     return sent_bool
 
 def remove_tweet(tweet):
     # Deleting from all tweets
-    with open("Tweets2.txt", "r") as f:
+    with open("Tweets.txt", "r") as f:
         lines = f.readlines()
         f.close()
-    with open("Tweets2.txt", "w") as f:
+    with open("Tweets.txt", "w") as f:
         for line in lines:
             if line != tweet:
                 f.write(line)
@@ -50,34 +50,18 @@ def remove_tweet(tweet):
 # Saving tweet in txt - these start at 0
 def save_sent_tweet(tweet):
 
-    archive = open('Sent_Tweets2.txt', 'a')
+    archive = open('Sent_Tweets.txt', 'a')
     archive.writelines(tweet)
     archive.close()
-
 
 ## CONNECTING TO TWITTER ###
 
 # Authenticate to Twitter
-auth = tweepy.OAuthHandler("OVKyfFEKOkF41d9S4aHiR8Of6",
-                           "VWFiNisJXp0wqMzrFTzGYlRdP69ALO77pAxls0XHnmarMuXbww")
-auth.set_access_token("1255949337269612546-uq4oaI8gzqYRo1JLdt6WWPqq8NqJf0",
-                      "bCBILV6NfUs84RAIYPDiLW8VM6t8mtFD2Wr99Rcsonsjn")
+auth = tweepy.OAuthHandler() #enter 0Auth details
+auth.set_access_token() #enter access token details
 
 # Connect to Twitter API
 api = tweepy.API(auth)
-
-'''
-# reading txt file with tweets
-filename = open('Tweets2.txt', 'r')
-all_tweets = filename.readlines()
-filename.close()
-tweet = all_tweets[0]
-
-print(tweet)
-print(too_long_bool(tweet))
-print(sent_tweet_bool(tweet))
-remove_tweet(tweet)
-'''
 
 ### TWEETING ###
 
@@ -85,7 +69,7 @@ bad_tweet = True
 
 while bad_tweet:
     # reading txt file with tweets
-    filename = open('Tweets2.txt', 'r')
+    filename = open('Tweets.txt', 'r')
     all_tweets = filename.readlines()
     filename.close()
     tweet = all_tweets[0]
